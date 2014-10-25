e27b3798a1a97e725d1a79bbef9fc2b144ca1b84
SpaceBug199 <filip.kov@gmail.com>
2014-10-25 20:13:03 +0200

    Parser
    Jos malo koda u parsery

--- generator/ucitaj.py
+++ generator/ucitaj.py
@@ -7,19 +7,18 @@
 #   [ ]     Generiraj popis automata prema glavnim stanjima
 #   [ ]     Generiraj automat za zadani regex
 #   [ ]     Razdovji stanja u zasebne liste
-#   [*]     Naprapi popis glavnih stanja
+#   [*]     Napravi popis glavnih stanja
+#   [*]     Popis lex jedinki za koristenje u lex analizatoru
 #
 #
 #
-#
-
 
 
 def ucitajUlaz():
     inputs = []
-    popis_mega_stanja = []
     regex = {}
     final = ''
+    # procitaj ulaznu datoteku definicije jezika, spremi svaki red kao novi clan liste inputs
     while True:
         try:
             inputs.append(raw_input())
@@ -27,35 +26,30 @@ def ucitajUlaz():
             final = '\n'.join(inputs)
             break
     print final
-    
-    print len(inputs)
+    #parsiraj ulaznu datoteku
+#    print len(inputs)  ## debug print
     for line in inputs:
+        # spremi stanja lex analizatora u listu stanja_lex_analizatora
         if "%X" in line:
+            stanja_lex_analizatora = line.split('%X')[1].strip().split(' ')
+        # spremi oznake lex jedinki u listu lex_jedinke
+        if "%L" in line:
+            lex_jedinke = line.split("%L")[1].strip().split(' ')
             break
+        # spremi definicije regex izraza u dict regex
         regex[line.split(' ')[0]] = line.split(' ')[1]
 
-    for key in regex.keys():  
+    # dopuni regularne izraze (zamjeni skracenice s punim regularnim izrazom)
+    for key in regex.keys():
        for reg in regex.keys():
                 if key in regex[reg]:
                     regex[reg] = regex[reg].replace(key, "(" + regex[key] + ")")
-                
-    for line in inputs:
-        if "%X" in line:
-            popis_mega_stanja = line.split('%X')[1].strip().split(' ')
-            
-    print popis_mega_stanja
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
+
+
+#    print lex_jedinke  ## debug print
+#    print stanja_lex_analizatora   ## debug print
+
+# ako se samostalno ucitava file pokreni ucitajUlaz()
 if __name__ == "__main__":
     ucitajUlaz()
