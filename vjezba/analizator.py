81e3dabb58476f6982407a9979df2dd6cbd6f40c
Dzaposlav <fdzapo@gmail.com>
2014-10-26 22:08:57 +0100

    Nije  testiran

--- vjezba/analizator.py
+++ vjezba/analizator.py
@@ -4,8 +4,8 @@ def prijelaz(Qs,z):
   Rs=[]
   for i in Qs:
     klj=i+'|'+z #stanje|znak   pretpostavio da dict prijelaza je oblika {'q|a':'q1,q2,q3 akcija? akcija?'
-    if(klj in PrijStanja[stanje]):
-      stanja=PrijStanja[stanje][klj].split(' ')[0].split(',') #stanja
+    if(klj in dicPrij):
+      stanja=dicPrij[klj].split(' ')[0].split(',') #stanja
       for j in stanja:
         Rs.append(j) #dodaj u skup stanja
   return Rs
@@ -20,8 +20,8 @@ def eOkruzenje(Qs):
   while(len(stog)!=0):
     s=stog.pop()  #za svako stanje ustogu e prijelaz
     klj=s+'|'+'$'
-    if(klj in PrijStanja[stanje]):
-      stanja=PrijStanja[stanje][klj].split(' ')[0].split(',')
+    if(klj in dicPrij):
+      stanja=dicPrij[klj].split(' ')[0].split(',')
       for g in stanja:
         if (g not in Rs):
           Rs.append(g) #ako nije u skupu dodaj ga
@@ -31,12 +31,11 @@ def eOkruzenje(Qs):
 def prijelazi(aut):
   dicPrijelazi={}
   red=aut.readline().rstrip()
-  pocS = red.split('|')[0]
   while(red):
     lRed=red.split(' ')
     dicPrijelazi[lRed[0]]=lRed[1]
     red=aut.readline().rstrip() #3 7 15 26 41
-  return dicPrijelazi,pocS
+  return dicPrijelazi
 
 def akc(aut):
   dicAkcije={}
@@ -79,7 +78,6 @@ brRedaka=1
 pocetak=0
 posljednji=0
 zavrsetak=0
-stanje=0
 
 input = open("tst.in","r")
 ulaz = input.read().replace('\n','\\''n')
@@ -89,19 +87,26 @@ dicPrij={}
 akcije={}
 
 automat = open('autom4.txt','r')
-sta = automat.readline().rstrip().split(' ') #stanja automata
-PrijStanja=[] #lista dictionarya u kojima su prijelazi
-pocStanja=[] #pocetno stanje za svako od stanja automata
-for i in range(len(sta)):
-  s = automat.readline().rstrip()
-  dicPrij,temp1 = prijelazi(automat) #ucitavanje prijelaza i pocetnog stanja za jedan e-NKA
-  pocStanja.append(temp1)
-  PrijStanja.append(dicPrij) #PrijStanja[stanje] referancira dictionary prijelaza ya trenutno stanje automata
+pocStanja={}
+sta1 = automat.readline().rstrip().split(' ') #stanja automata
+pocStanja={}
+for i in range(len(sta1)):
+  klj = sta1[i].split('|')[0]
+  poc =sta1[i].split('|')[1].split(',')
+  pocStanja[klj]=poc
+  if (1 in poc):
+    stanje=klj
+print pocStanja
+
+
+
+dicPrij = prijelazi(automat)
+automat.readline() #ucitavanje prijelaza i pocetnog stanja za jedan e-NKA
 akcije=akc(automat) #ucitavanje akcija
-print PrijStanja
+
 
 while (zavrsetak<len(ulaz)):
-  Q=[pocStanja[stanje]]
+  Q=pocStanja[stanje]
   Q=eOkruzenje(Q)
   izraz=''
   while(Q):
