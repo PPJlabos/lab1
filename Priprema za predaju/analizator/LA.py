47c6bffd82e9415abe888ff0c3d0aa520da07e7b
Dzaposlav <fdzapo@gmail.com>
2014-11-01 19:53:55 +0100

    nije se save-alo

--- Priprema za predaju/analizator/LA.py
+++ Priprema za predaju/analizator/LA.py
@@ -2,6 +2,9 @@ import sys
 
 def prijelaz(Qs,z):
   Rs=[]
+  if(z=='$'):
+    z='$$'
+
   for i in Qs:
     klj=i+'|'+z #stanje|znak   pretpostavio da dict prijelaza je oblika {'q|a':'q1,q2,q3 akcija? akcija?'
     if(klj in dicPrij):
